2891fa5f26660c08bf8fec00300f3965d6269e85
Pova <roman.kitsela@gmail.com>
2024-12-10 17:24:43 +0000

    add day 3 solution and start day 4

--- solutions/day_03.py
+++ solutions/day_03.py
@@ -1,14 +1,18 @@
-with open("test_inputs/day_03.txt", "r") as file:
+with open("real_inputs/day_03.txt", "r") as file:
 
     input_data = [line.strip("\n") for line in file.readlines()]
     
-print(input_data)
+# print(f"input_data: {input_data}")
+print(f"lines of input data: {len(input_data)}")
+print('-'*30)
+
+total_input_data = ''.join(input_data)
 
 # part 1
 
-current_sum = 0
-idx = 0
 # # ------------------------------------------
+# current_sum = 0
+# idx = 0
 # check if current char is 'm' 
     # if not: increment idx (by 1)
     # if so:
@@ -21,12 +25,11 @@ idx = 0
 # # ------------------------------------------
 
 def find_string_in_string(substring, main_string):
-    counter = 0
     indices = []
     idx = 0
     found_idx = main_string.find(substring, idx)
     
-    while found_idx != -1 and counter < 100:
+    while found_idx != -1:
         
         indices.append(found_idx)
         idx = found_idx + len(substring)
@@ -34,10 +37,175 @@ def find_string_in_string(substring, main_string):
         
         if idx >= len(main_string):
             break
-        
-        counter += 1
     
     return indices
 
-input_str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-print(find_string_in_string("mul", input_str))
+
+total_sum = 0
+indices_to_check = find_string_in_string("mul", total_input_data)
+
+print_rejection = False
+print_acceptance = False
+
+for idx in indices_to_check:
+    
+    # print(f'checking idx: {idx}')
+    
+    current_idx = idx + 3
+    
+    if total_input_data[current_idx] != '(':
+        if print_rejection:
+            print(f'rejecting: {total_input_data[idx:current_idx+1]}')
+        continue
+    
+    current_idx += 1
+    # ----------------------------------------
+    first_num_len, first_num_left_idx = 0, current_idx
+    while total_input_data[current_idx].isnumeric():
+        current_idx += 1
+        first_num_len += 1
+        
+    if first_num_len > 3 or first_num_len == 0:
+        if print_rejection:
+            print(f'rejecting: {total_input_data[idx:current_idx+1]}')
+        continue
+    else:
+        first_num = int(total_input_data[first_num_left_idx:current_idx])
+    # ----------------------------------------
+    
+    if total_input_data[current_idx] != ',':
+        if print_rejection:
+            print(f'rejecting: {total_input_data[idx:current_idx+1]}')
+        continue
+    
+    current_idx += 1
+    
+    second_num_len, second_num_left_idx = 0, current_idx
+    while total_input_data[current_idx].isnumeric():
+        current_idx += 1
+        second_num_len += 1
+    
+    if second_num_len > 3 or second_num_len == 0:
+        if print_rejection:
+            print(f'rejecting: {total_input_data[idx:current_idx+1]}')
+        continue
+    else:
+        second_num = int(total_input_data[second_num_left_idx:current_idx])
+        
+    if total_input_data[current_idx] != ')':
+        if print_rejection:
+            print(f'rejecting: {total_input_data[idx:current_idx+1]}')
+        continue
+    
+    if print_acceptance:
+        print(f'got a valid command starting at: {idx}: {total_input_data[idx:current_idx+1]}')
+    total_sum += first_num*second_num
+        
+print(f'total = {total_sum}')
+
+# 23465714 (incorrect - too low)
+# 145501706 (incorrect - too low)
+# 23465714 (counter while limit bug)
+# 178794710
+
+# Part 2 
+
+do_breakpoints = [0] + find_string_in_string("do()", total_input_data)
+dont_breakpoints = find_string_in_string("don't()", total_input_data)
+
+do_breakpoints = [idx+4 for idx in do_breakpoints]
+dont_breakpoints = [idx+7 for idx in dont_breakpoints]
+
+# print(total_input_data)
+# print(do_breakpoints)
+# print(dont_breakpoints)
+
+def check_mul_is_active(idx):
+    
+    best_do_idx = None
+    best_dont_idx = None
+    
+    for do_idx in do_breakpoints:
+        if do_idx <= idx:
+            best_do_idx = do_idx
+        else:
+            break
+        
+    for dont_idx in dont_breakpoints:
+        if dont_idx <= idx:
+            best_dont_idx = dont_idx
+        else:
+            break
+    
+    if best_dont_idx is None:
+        return True
+    elif best_do_idx > best_dont_idx:
+        return True
+    else:
+        return False
+
+indices_to_check = find_string_in_string("mul", total_input_data) # wasteful dont need to run this again
+
+total_sum = 0
+
+print_rejection = False
+print_acceptance = False
+
+for idx in indices_to_check:
+    
+    # print(f'checking idx: {idx}')
+    
+    current_idx = idx + 3
+    
+    if total_input_data[current_idx] != '(':
+        if print_rejection:
+            print(f'rejecting: {total_input_data[idx:current_idx+1]}')
+        continue
+    
+    current_idx += 1
+    # ----------------------------------------
+    first_num_len, first_num_left_idx = 0, current_idx
+    while total_input_data[current_idx].isnumeric():
+        current_idx += 1
+        first_num_len += 1
+        
+    if first_num_len > 3 or first_num_len == 0:
+        if print_rejection:
+            print(f'rejecting: {total_input_data[idx:current_idx+1]}')
+        continue
+    else:
+        first_num = int(total_input_data[first_num_left_idx:current_idx])
+    # ----------------------------------------
+    
+    if total_input_data[current_idx] != ',':
+        if print_rejection:
+            print(f'rejecting: {total_input_data[idx:current_idx+1]}')
+        continue
+    
+    current_idx += 1
+    
+    second_num_len, second_num_left_idx = 0, current_idx
+    while total_input_data[current_idx].isnumeric():
+        current_idx += 1
+        second_num_len += 1
+    
+    if second_num_len > 3 or second_num_len == 0:
+        if print_rejection:
+            print(f'rejecting: {total_input_data[idx:current_idx+1]}')
+        continue
+    else:
+        second_num = int(total_input_data[second_num_left_idx:current_idx])
+        
+    if total_input_data[current_idx] != ')':
+        if print_rejection:
+            print(f'rejecting: {total_input_data[idx:current_idx+1]}')
+        continue
+    
+    if print_acceptance:
+        print(f'got a valid command starting at: {idx}: {total_input_data[idx:current_idx+1]}')
+    
+    mul_is_active = check_mul_is_active(idx)
+    if mul_is_active:
+        total_sum += first_num*second_num
+        
+print(f'total = {total_sum}')
---
+++ solutions/day_04.py
@@ -0,0 +1,161 @@
+
+with open("test_inputs/day_04.txt", "r") as file:
+
+    input_data = [line.strip("\n") for line in file.readlines()]
+
+print('\n')
+print('Starting board:\n')
+print('\n'.join([''.join(line) for line in input_data]))
+print()
+
+check_horizontal = False
+check_vertical = False
+check_diagonal_1 = True
+check_diagonal_2 = False
+
+print_no_finds = False
+
+# idea: scan across in 6 days:
+
+# Horizontal
+# ----------------------
+# left --> right
+# right --> left
+# ----------------------
+
+# Vertical
+# ----------------------
+# up --> down
+# down --> up
+# ----------------------
+
+# Diagonal 1 (/)
+# ----------------------
+# down left --> up right
+# up right --> down left
+# ----------------------
+
+# Diagonal 2 (\)
+# ----------------------
+# up left --> down right
+# down right --> up left
+# ----------------------
+
+# part 1
+
+boolean_board = [[False for col_count in range(len(input_data[0]))] for row_count in range(len(input_data))]
+
+if check_horizontal:
+    # Horizontal (left --> right)
+    for y_idx in range(len(input_data)):
+        
+        text_line = input_data[y_idx]
+        
+        if text_line.find('XMAS') == -1:
+            if print_no_finds:
+                print(f'no horizontal (left-to-right) XMAS on line: {y_idx}')
+            continue
+        else:
+            for x_idx in range(len(input_data[0])-3):
+                if text_line[x_idx:x_idx+4] == 'XMAS':
+                    for i in range(x_idx,x_idx+4):
+                        boolean_board[y_idx][i] = True
+
+    # Horizontal (right --> left)
+    for y_idx in range(len(input_data)):
+        
+        text_line = input_data[y_idx][::-1]
+        
+        if text_line.find('XMAS') == -1:
+            if print_no_finds:
+                print(f'no horizontal (right-to-left) XMAS on line: {y_idx}')
+            continue
+        else:
+            for x_idx in range(len(input_data[0])-3):
+                if text_line[x_idx:x_idx+4] == 'XMAS':
+                    for i in range(x_idx,x_idx+4):
+                        boolean_board[y_idx][len(input_data[0])-1-i] = True
+                        
+if check_vertical:
+    # Vertical (up --> down)
+    for x_idx in range(len(input_data[0])):
+        
+        text_line = ''.join([line[x_idx] for line in input_data])
+        
+        if text_line.find('XMAS') == -1:
+            if print_no_finds:
+                print(f'no vertical (up-to-down) XMAS on line: {x_idx}')
+            continue
+        else:
+            for y_idx in range(len(input_data)-3):
+                if text_line[y_idx:y_idx+4] == 'XMAS':
+                    for j in range(y_idx,y_idx+4):
+                        boolean_board[j][x_idx] = True
+    
+    # Horizontal (down --> up)
+    for x_idx in range(len(input_data[0])):
+        
+        text_line = ''.join([line[x_idx] for line in input_data])[::-1]
+        
+        if text_line.find('XMAS') == -1:
+            if print_no_finds:
+                print(f'no horizontal (down-to-up) XMAS on line: {y_idx}')
+            continue
+        else:
+            for y_idx in range(len(input_data)-3):
+                if text_line[y_idx:y_idx+4] == 'XMAS':
+                    for j in range(y_idx,y_idx+4):
+                        boolean_board[len(input_data)-1-j][x_idx] = True
+
+# Diagonal 1 (/)
+# ----------------------
+# down left --> up right
+# up right --> down left
+# ----------------------
+                        
+if check_diagonal_1:
+    # Diagonal 1 (down left --> up right)
+    for y_idx in range(len(input_data)):
+        index_list = []
+        
+        for x_idx in range(y_idx+1):
+            
+            index_list.append((x_idx,y_idx-x_idx))
+            
+        print(y_idx, index_list)
+    #         # text_line = ''.join()
+        
+    #     text_line = ''.join([line[x_idx] for line in input_data])
+        
+    #     if text_line.find('XMAS') == -1:
+    #         if print_no_finds:
+    #             print(f'no vertical (up-to-down) XMAS on line: {x_idx}')
+    #         continue
+    #     else:
+    #         for y_idx in range(len(input_data)-3):
+    #             if text_line[y_idx:y_idx+4] == 'XMAS':
+    #                 for j in range(y_idx,y_idx+4):
+    #                     boolean_board[j][x_idx] = True
+    
+    # # Horizontal (down --> up)
+    # for x_idx in range(len(input_data[0])):
+        
+    #     text_line = ''.join([line[x_idx] for line in input_data])[::-1]
+        
+    #     if text_line.find('XMAS') == -1:
+    #         if print_no_finds:
+    #             print(f'no horizontal (down-to-up) XMAS on line: {y_idx}')
+    #         continue
+    #     else:
+    #         for y_idx in range(len(input_data)-3):
+    #             if text_line[y_idx:y_idx+4] == 'XMAS':
+    #                 for j in range(y_idx,y_idx+4):
+    #                     boolean_board[len(input_data)-1-j][x_idx] = True
+
+                        
+                                      
+print()
+print('Boolean board:\n')
+boolean_board_str = [['X' if bool else '.' for bool in bool_list] for bool_list in boolean_board]
+print('\n'.join([''.join(line) for line in boolean_board_str]))
+print()
