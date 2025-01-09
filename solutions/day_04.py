a59eb0fa9d93ee1882f9cab07ce500f3cb90fb61
RomanKitsela <roman.kitsela@ambersideenergy.com>
2025-01-09 11:33:27 +0000

    day 4 solutions - attempt 1

--- solutions/day_04.py
+++ solutions/day_04.py
@@ -114,46 +114,77 @@ if check_vertical:
 # ----------------------
                         
 if check_diagonal_1:
-    # Diagonal 1 (down left --> up right)
-    for y_idx in range(len(input_data)):
-        index_list = []
-        
-        for x_idx in range(y_idx+1):
-            
-            index_list.append((x_idx,y_idx-x_idx))
-            
-        print(y_idx, index_list)
-    #         # text_line = ''.join()
-        
-    #     text_line = ''.join([line[x_idx] for line in input_data])
-        
-    #     if text_line.find('XMAS') == -1:
-    #         if print_no_finds:
-    #             print(f'no vertical (up-to-down) XMAS on line: {x_idx}')
-    #         continue
-    #     else:
-    #         for y_idx in range(len(input_data)-3):
-    #             if text_line[y_idx:y_idx+4] == 'XMAS':
-    #                 for j in range(y_idx,y_idx+4):
-    #                     boolean_board[j][x_idx] = True
     
-    # # Horizontal (down --> up)
-    # for x_idx in range(len(input_data[0])):
+    for diag_value in range(len(input_data) + len(input_data[0]) - 1):
         
-    #     text_line = ''.join([line[x_idx] for line in input_data])[::-1]
+        print(f'diag_value = {diag_value}')
+        diagonal_positions = []
+        diagonal_characters = []
+        y_idx = diag_value if diag_value < len(input_data) else len(input_data)-1
         
-    #     if text_line.find('XMAS') == -1:
-    #         if print_no_finds:
-    #             print(f'no horizontal (down-to-up) XMAS on line: {y_idx}')
-    #         continue
-    #     else:
-    #         for y_idx in range(len(input_data)-3):
-    #             if text_line[y_idx:y_idx+4] == 'XMAS':
-    #                 for j in range(y_idx,y_idx+4):
-    #                     boolean_board[len(input_data)-1-j][x_idx] = True
-
-                        
-                                      
+        for x_idx in range(diag_value-y_idx,len(input_data[0])):
+            if diag_value-x_idx < 0:
+                break
+            
+            diagonal_positions.append((x_idx, diag_value-x_idx))
+            diagonal_characters.append(input_data[diag_value-x_idx][x_idx])
+            # print(f'x_idx, y_idx = {x_idx}, {y_idx} ')
+        
+        if len(diagonal_positions) < 4:
+            continue
+         
+        print(diagonal_positions, len(diagonal_positions))
+        text_line = ''.join(diagonal_characters)
+        print(text_line)
+        
+        if text_line.find('XMAS') == -1:
+            if print_no_finds:
+                print(f'no diagonal (/) XMAS on line: {diagonal_positions}')
+            continue
+        else:
+            for y_idx in range(len(input_data)-3):
+                if text_line[y_idx:y_idx+4] == 'XMAS':
+                    for j in range(y_idx,y_idx+4):
+                        boolean_board[j][x_idx] = True
+        
+    # # Diagonal 1 (down left --> up right)
+    # for y_idx in range(len(input_data)):
+    #     index_list = []
+        
+    #     for x_idx in range(y_idx+1):
+            
+    #         index_list.append((x_idx,y_idx-x_idx))
+            
+    #     print(y_idx, index_list)
+    # #         # text_line = ''.join()
+        
+    # #     text_line = ''.join([line[x_idx] for line in input_data])
+        
+    # #     if text_line.find('XMAS') == -1:
+    # #         if print_no_finds:
+    # #             print(f'no vertical (up-to-down) XMAS on line: {x_idx}')
+    # #         continue
+    # #     else:
+    # #         for y_idx in range(len(input_data)-3):
+    # #             if text_line[y_idx:y_idx+4] == 'XMAS':
+    # #                 for j in range(y_idx,y_idx+4):
+    # #                     boolean_board[j][x_idx] = True
+    
+    # # # Horizontal (down --> up)
+    # # for x_idx in range(len(input_data[0])):
+        
+    # #     text_line = ''.join([line[x_idx] for line in input_data])[::-1]
+        
+    # #     if text_line.find('XMAS') == -1:
+    # #         if print_no_finds:
+    # #             print(f'no horizontal (down-to-up) XMAS on line: {y_idx}')
+    # #         continue
+    # #     else:
+    # #         for y_idx in range(len(input_data)-3):
+    # #             if text_line[y_idx:y_idx+4] == 'XMAS':
+    # #                 for j in range(y_idx,y_idx+4):
+    # #                     boolean_board[len(input_data)-1-j][x_idx] = True
+                  
 print()
 print('Boolean board:\n')
 boolean_board_str = [['X' if bool else '.' for bool in bool_list] for bool_list in boolean_board]
